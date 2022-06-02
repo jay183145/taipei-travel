eafd061a8232d54b9f9c2ceb4891e8446bfb6e3b
jay183145 <jay183145@gmail.com>
2022-06-02 22:45:06 +0800

    vue-axios

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import axios from 'axios'
 import VueAxios from 'vue-axios'
+
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
