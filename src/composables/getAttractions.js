aba5552d61058a4e698b162b847b2bc8749e4a4d
jay183145 <jay183145@gmail.com>
2022-06-03 21:32:26 +0800

    cros solve by vite

--- src/composables/getAttractions.js
+++ src/composables/getAttractions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 import { ref } from 'vue';
 
 const getAttractions = ()=> {
@@ -7,36 +7,37 @@ const getAttractions = ()=> {
   const attractionWithOrganizer = ref(null)
   const error = ref(null)
 
-  //set header
-  const config = {
-      headers: {
-        header1: 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
-      }
-    }
-    // // use cors-anywhere to fetch api data
-    const corsURL = 'https://cors-anywhere.herokuapp.com/';
-    const apiURL = 'https://www.travel.taipei/open-api/zh-tw/Events/Activity?begin=2022-06-02&end=2022-06-02&page=1'
-    // Make a request for a user with a given ID
-    
-    const load = async () => {
-      try {
-          axios.get(`${corsURL}${apiURL}`, config)
-            .then((response) => {
-              attractions.value = response.data.data
-              // console.log(response.data.data)
+//set header
+const config = {
+  headers: {
+    header1: 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36',
+  }
+}
+  const apiURL = '/api/zh-tw/Events/Activity?begin=2022-06-02&end=2022-06-02&page=1'
+  // Make a request for a user with a given ID
+  
+  const load = async () => {
+    try {
+      await axios.get(apiURL, config)
+        .then((response) => {
 
-              attractionWithOrganizer.value = response.data.data.filter((hasOrg) => hasOrg.organizer)
-              // console.log(attractionWithOrganizer)
-            })
-            .catch((error) => {
-              console.log(error);
-            })
-      }
-      catch (err) {
-        error.value = err.message
-      }
-    }
+          //get all datas in the setting time
+          attractions.value = response.data.data
+          console.log(response.data.data)
 
-    return { attractionWithOrganizer, attractions, error, load }
+          //get the datas which has organizer in the setting time
+          attractionWithOrganizer.value = response.data.data.filter((hasOrg) => hasOrg.organizer)
+          console.log(attractionWithOrganizer)
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+    catch (err) {
+      error.value = err.message
+    }
+  }
+
+  return { attractionWithOrganizer, attractions, error, load }
 }
 export default getAttractions
--- vite.config.js
+++ vite.config.js
@@ -22,5 +22,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.travel.taipei/open-api",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
