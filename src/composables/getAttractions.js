7f5760e4f86783756aa1aef112b21f8a4fcbaadd
藍天駿 <jay183145@gmail.com>
2022-06-07 16:15:17 +0800

    write all the commit

--- src/composables/getAttractions.js
+++ src/composables/getAttractions.js
@@ -22,36 +22,53 @@ const config = {
     try {
       await axios.get(apiURL, config)
         .then((response) => {
-
+          //----------------------------------- get the origin data ----------------------------------------// 
           //get all datas in the setting time
           // attractions.value = response.data
           // console.log(attractions)
 
-          //get the datas which has organizer in the setting time
+          // --------------------------get the datas which has organizer---------------
           attractionWithOrganizer.value = response.data.data.filter((hasOrg) => hasOrg.organizer)
-          console.log(attractionWithOrganizer)
-          console.log(Object.values(attractionWithOrganizer._rawValue))
+          // -----> log data
+          // console.log(attractionWithOrganizer)
+          // console.log(Object.values(attractionWithOrganizer._rawValue))
           //object split and deepcopy
           let organizerSplit = _.cloneDeep(attractionWithOrganizer.value)
-          // all organizer in to array
+          //---------xxxxxxxxx---------------- get the data ------------------xxxxxxxxxxxx---------// 
+
+          //-----------------------------------  filter data  ----------------------------------------//
+          // split("、") array length array
           let organizerLengthArray = []
+          //iter through Array of Object
           organizerSplit.forEach(element => {
-            let organizerArray = []
-            
+            // split("、") array
+            let organizerArray = []    
             Object.entries(element).forEach(([key, value]) => {
               // console.log(`${key}: ${value}`)
               if ( key === "organizer" && value.includes("、")) {
                 organizerArray = value.split("、")
                 
               } 
-            })
-                     
-            console.log(element)
-            console.log(organizerArray)
-
+            }) 
+            //------->  log each object         
+            // console.log(element)
+            // console.log(organizerArray)
+            // push split("、") array  to an array 
             organizerLengthArray.push(organizerArray)
           })
-          console.log(organizerLengthArray)
+        //----> log  split("、") array length array
+        // console.log(organizerLengthArray)
+
+        for (let i = 0; i <= organizerLengthArray.length-1; i++ ) {
+          if ( organizerLengthArray[i].length === 0  ) {
+          } else {
+            // console.log(organizerSplit[i])
+
+            // organizerSplit.push(organizerSplit[i]) 
+          }
+        }
+        console.log(organizerSplit)
+        //------------xxxxxxxxxxx---------------  filter data  ------------xxxxxxxx--------------//
         })
         .catch((err) => {
           error.value = err.message
